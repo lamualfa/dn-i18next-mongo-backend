d97f57c3434ff7cc411dbaf723bddf23752c95cd
Laode Muhammad Al Fatih <lamualfa@gmail.com>
2020-10-26 21:43:38 +0700

    Update Mongo Driver version

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { MongoClient, ClientOptions, Collection } from 'https://deno.land/x/mongo@v0.7.0/mod.ts';
+export { MongoClient, ClientOptions, Collection } from 'https://deno.land/x/mongo@v0.13.0/mod.ts';
 export { assertEquals } from 'https://deno.land/std/testing/asserts.ts';
